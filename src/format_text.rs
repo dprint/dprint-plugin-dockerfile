9066a84e812aa0484fc27892a6401d80a74dbe7c
David Sherret <dsherret@gmail.com>
2022-03-28 20:56:48 -0400

    refactor: upgrade to dprint-core 0.54.1

--- src/format_text.rs
+++ src/format_text.rs
@@ -2,25 +2,28 @@ use anyhow::Result;
 use dockerfile_parser::Dockerfile;
 use dprint_core::configuration::resolve_new_line_kind;
 use dprint_core::formatting::PrintOptions;
+use dprint_core::plugins::FormatResult;
 use std::path::Path;
 
 use crate::configuration::Configuration;
 use crate::generation::generate;
 
-pub fn format_text(_file_path: &Path, text: &str, config: &Configuration) -> Result<String> {
+pub fn format_text(_file_path: &Path, text: &str, config: &Configuration) -> FormatResult {
   let node = parse_node(text)?;
 
-  Ok(dprint_core::formatting::format(
-    || generate(&node, text, config),
-    config_to_print_options(text, config),
-  ))
+  let result = dprint_core::formatting::format(|| generate(&node, text, config), config_to_print_options(text, config));
+  if result == text {
+    Ok(None)
+  } else {
+    Ok(Some(result))
+  }
 }
 
 #[cfg(feature = "tracing")]
 pub fn trace_file(_file_path: &Path, text: &str, config: &Configuration) -> dprint_core::formatting::TracingResult {
   let node = parse_node(text).unwrap();
 
-  dprint_core::formatting::trace_printing(|| generate(node, text, config), config_to_print_options(text, config))
+  dprint_core::formatting::trace_printing(|| generate(&node, text, config), config_to_print_options(text, config))
 }
 
 fn parse_node(text: &str) -> Result<Dockerfile> {
