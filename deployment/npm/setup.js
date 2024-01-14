019e1273a3c285171e00a9fa80914099e7d6b336
David Sherret <dsherret@gmail.com>
2024-01-13 21:01:23 -0500

    chore: update internal formatting plugins

--- deployment/npm/setup.js
+++ deployment/npm/setup.js
@@ -6,10 +6,10 @@ const wasmPath = path.join(__dirname, "../../target/wasm32-unknown-unknown/relea
 fs.copyFileSync(wasmPath, path.join(__dirname, "plugin.wasm"));
 
 if (args.length > 0) {
-    // update the version based on the first argument
-    const packageJsonPath = path.join(__dirname, "package.json");
-    const packageJsonText = fs.readFileSync(packageJsonPath, "utf8");
-    const packageJson = JSON.parse(packageJsonText);
-    packageJson.version = args[0];
-    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, undefined, 2) + "\n");
+  // update the version based on the first argument
+  const packageJsonPath = path.join(__dirname, "package.json");
+  const packageJsonText = fs.readFileSync(packageJsonPath, "utf8");
+  const packageJson = JSON.parse(packageJsonText);
+  packageJson.version = args[0];
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, undefined, 2) + "\n");
 }
